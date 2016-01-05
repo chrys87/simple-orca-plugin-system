1c139477800a9966f646d5a069165214fc806acf
chrys <chrys87@web.de>
2016-01-06 00:05:31 +0100

    improve weather plugin

--- plugins/plugins-available/weather-startnotify+control+alt+w.sh
+++ plugins/plugins-available/weather-startnotify+control+alt+w.sh
@@ -2,9 +2,12 @@
 # this presents the weather
 # change the city to yours (see manual)
 #for this you need weather-utils
+# this is the default of the software
 if [ -e /usr/bin/weather ]; then
     weather nuernberg | tail -n  +5
 fi
+
+#sadly in arch it is renamed caused by an fileconflict so we have to check
 if [ -e /usr/bin/weather-report ]; then
     weather-report nuernberg | tail -n  +5
 fi
