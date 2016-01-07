e12a5c3e553fbe21c6a5068a70975865f0c9d0ff
chrys <chrys87@web.de>
2016-01-08 00:37:43 +0100

    rework of settings logic

--- SOPS/SimplePluginLoader.py
+++ SOPS/SimplePluginLoader.py
@@ -27,96 +27,117 @@ def outputMessage(Message):
     if (orca.settings.enableBraille):
         orca.braille.displayMessage(Message)
 
-def SetupShortcutAndHandle(fun, scriptname, key, shiftkey, ctrlkey, altkey):
-    if not fun:
-        return
-    if (key ==	 '') or (key ==	 'ERROR'):
-        return
-    inputEventHandlers.append(orca.input_event.InputEventHandler(fun, scriptname))   
+def SetupShortcutAndHandle(fun, settings):
+    inputEventHandlers.append(orca.input_event.InputEventHandler(fun, settings['scriptname'], settings))
     # just the orca modifier
-    if not shiftkey and not ctrlkey and not altkey:
-        myKeyBindings.add(orca.keybindings.KeyBinding(key, orca.keybindings.defaultModifierMask, orca.keybindings.ORCA_MODIFIER_MASK, inputEventHandlers[len(inputEventHandlers) - 1]))
+    if not settings['shiftkey'] and not settings['ctrlkey'] and not settings['altkey']:
+        myKeyBindings.add(orca.keybindings.KeyBinding(settings['key'], orca.keybindings.defaultModifierMask, orca.keybindings.ORCA_MODIFIER_MASK, inputEventHandlers[len(inputEventHandlers) - 1]))
     # orca + alt
-    if not shiftkey and not ctrlkey and altkey:
-        myKeyBindings.add(orca.keybindings.KeyBinding(key, orca.keybindings.defaultModifierMask, orca.keybindings.ORCA_ALT_MODIFIER_MASK, inputEventHandlers[len(inputEventHandlers) - 1]))
+    if not settings['shiftkey'] and not settings['ctrlkey'] and settings['altkey']:
+        myKeyBindings.add(orca.keybindings.KeyBinding(settings['key'], orca.keybindings.defaultModifierMask, orca.keybindings.ORCA_ALT_MODIFIER_MASK, inputEventHandlers[len(inputEventHandlers) - 1]))
     # orca + CTRL
-    if not shiftkey and ctrlkey and not altkey:
-        myKeyBindings.add(orca.keybindings.KeyBinding(key, orca.keybindings.defaultModifierMask, orca.keybindings.ORCA_CTRL_MODIFIER_MASK, inputEventHandlers[len(inputEventHandlers) - 1]))
+    if not settings['shiftkey'] and settings['ctrlkey'] and not settings['altkey']:
+        myKeyBindings.add(orca.keybindings.KeyBinding(settings['key'], orca.keybindings.defaultModifierMask, orca.keybindings.ORCA_CTRL_MODIFIER_MASK, inputEventHandlers[len(inputEventHandlers) - 1]))
     # orca + alt + CTRL
-    if not shiftkey and ctrlkey and altkey:
-        myKeyBindings.add(orca.keybindings.KeyBinding(key, orca.keybindings.defaultModifierMask, orca.keybindings.ORCA_CTRL_ALT_MODIFIER_MASK, inputEventHandlers[len(inputEventHandlers) - 1]))
+    if not settings['shiftkey'] and settings['ctrlkey'] and settings['altkey']:
+        myKeyBindings.add(orca.keybindings.KeyBinding(settings['key'], orca.keybindings.defaultModifierMask, orca.keybindings.ORCA_CTRL_ALT_MODIFIER_MASK, inputEventHandlers[len(inputEventHandlers) - 1]))
     # orca + shift
-    if shiftkey and not ctrlkey and not altkey:
-        myKeyBindings.add(orca.keybindings.KeyBinding(key, orca.keybindings.defaultModifierMask, orca.keybindings.ORCA_SHIFT_MODIFIER_MASK, inputEventHandlers[len(inputEventHandlers) - 1]))
+    if settings['shiftkey'] and not settings['ctrlkey'] and not settings['altkey']:
+        myKeyBindings.add(orca.keybindings.KeyBinding(settings['key'], orca.keybindings.defaultModifierMask, orca.keybindings.ORCA_SHIFT_MODIFIER_MASK, inputEventHandlers[len(inputEventHandlers) - 1]))
     functions.append(fun)
     orca.settings.keyBindingsMap["default"] = myKeyBindings
 
 def id_generator(size=7, chars=string.ascii_letters):
     return ''.join(random.choice(chars) for _ in range(size))
 
-def parseFileName(filepath):
+def initSettings():
+    settings={
+    'filepath':'',
+    'scriptname':'',
+    'functionname':'',
+    'key':'',
+    'shiftkey':False,
+    'ctrlkey':False,
+    'altkey':False,
+    'startnotify':False,
+    'stopnotify':False,
+    'blockcall':False,
+    'showstderr':False,
+    }
+    return settings
+
+def parseFileName(filepath, settings):
     try:
         filename = os.path.basename(filepath) #filename
         filename = os.path.splitext(filename)[0] #remove extension if we have one
         #remove scriptname seperated by -
         filenamehelper = filename.split('-')
         filename = filenamehelper[len(filenamehelper) - 1 ]
-        scriptname = 'NoNameAvailable'
+        settings['file'] = filepath
+        settings['scriptname'] = 'NoNameAvailable'
         if len(filenamehelper) == 2:
-            scriptname = filenamehelper[0]
+            settings['scriptname'] = filenamehelper[0]
         #now get shortcuts seperated by +
         filenamehelper = filename.split('+')
-        key = filenamehelper[len(filenamehelper) - 1]
-        shiftkey = 'shift' in filenamehelper
-        ctrlkey = 'control' in filenamehelper
-        altkey = 'alt' in filenamehelper
-        startnotify = 'startnotify' in filenamehelper
-        stopnotify = 'stopnotify' in filenamehelper
-        blockcall = 'blockcall' in filenamehelper
-        showstderr = 'showstderr' in filenamehelper
-        if len(key) != 1: #for now no special keys, but more valid data
+        settings['key'] = filenamehelper[len(filenamehelper) - 1]
+        settings['shiftkey'] = 'shift' in filenamehelper
+        settings['ctrlkey'] = 'control' in filenamehelper
+        settings['altkey'] = 'alt' in filenamehelper
+        settings['startnotify'] = 'startnotify' in filenamehelper
+        settings['stopnotify'] = 'stopnotify' in filenamehelper
+        settings['blockcall'] = 'blockcall' in filenamehelper
+        settings['showstderr'] = 'showstderr' in filenamehelper
+        if len(settings['key']) != 1: #for now no special keys, but more valid data
         # for the return I realy should use a dict
-            return '', 'ERROR', False, False, False, False, False, False, False 
-        return scriptname, key, shiftkey, ctrlkey, altkey, startnotify, stopnotify, blockcall, showstderr
+            settings = initSettings()
+            settings['key'] = 'ERROR'
+            return settings
+#            return '', 'ERROR', False, False, False, False, False, False, False 
+        return settings
+#        return scriptname, key, shiftkey, ctrlkey, altkey, startnotify, stopnotify, blockcall, showstderr
     except:
-        return '', 'ERROR', False, False, False, False, False, False, False
+        settings = initSettings()
+        settings['key'] = 'ERROR'
+        return settings
+        #return '', 'ERROR', False, False, False, False, False, False, False
 
-def buildScriptFunctions(fun_name, currscript, scriptname, startnotify, stopnotify, blockcall, showstderr):
-    currscript = "\'\"" + currscript + "\"\'"
-    fun_body = "def " + fun_name + "(script, inputEvent=None):\n"
-    if blockcall:
-       scriptname = "blocking " + scriptname 
-    if startnotify:
+def buildScriptFunctions(settings):
+    currscript = "\'\"" + settings['file'] + "\"\'"
+    fun_body = "def " + settings['functionname'] + "(script, inputEvent=None):\n"
+    scriptname = settings['scriptname']
+    if settings['blockcall']:
+       scriptname = "blocking " + scriptname
+    if settings['startnotify']:
         fun_body +="  outputMessage('start " + scriptname + "')\n"    
     fun_body +="  p = Popen(" + currscript + ", stdout=PIPE, stderr=PIPE, shell=True)\n"
     fun_body +="  stdout, stderr = p.communicate()\n"
     fun_body +="  message = ''\n"
     fun_body +="  if stdout:\n"
     fun_body +="    message += str(stdout, \"utf-8\")\n"
-    fun_body +="  if showstderr and stderr:\n"
+    fun_body +="  if " + str(settings['showstderr']) +" and stderr:\n"
     fun_body +="    message += ' error: ' + str(stderr, \"utf-8\")\n"
     fun_body +="  outputMessage( message)\n"
-    if stopnotify:
+    if settings['stopnotify']:
         fun_body +="  outputMessage('finish " + scriptname + "')\n"
     fun_body +="  return True\n\n"
-    fun_body +="def " + fun_name + "T(script, inputEvent=None):\n"
-    fun_body +="  _thread.start_new_thread("+fun_name + ",(script, inputEvent,))\n\n"
+    fun_body +="def " + settings['functionname'] + "T(script, inputEvent=None):\n"
+    fun_body +="  _thread.start_new_thread("+ settings['functionname'] + ",(script, inputEvent))\n\n"
     return fun_body
 
 if not loaded:
     scriptlist = glob.glob(scriptrepo+'*')
     for currscript in scriptlist:
-        scriptname, key, shiftkey, ctrlkey, altkey, startnotify, stopnotify, blockcall, showstderr = \
-          parseFileName	(currscript)
-        if key != 'ERROR':
-            fun_name = ''
-            while fun_name == '' or fun_name + 'T' in globals() or fun_name in globals():
-                fun_name = id_generator()
-            exec(buildScriptFunctions(fun_name, currscript, scriptname, startnotify, stopnotify, blockcall, showstderr))
-            if blockcall:
-                fun = globals()[fun_name]
+        settings = initSettings()
+        settings = parseFileName(currscript, settings)
+        if not settings['key'] in ['','ERROR']:
+            settings['functionname'] = ''
+            while settings['functionname'] == '' or settings['functionname'] + 'T' in globals() or settings['functionname'] in globals():
+                settings['functionname'] = id_generator()
+            exec(buildScriptFunctions(settings))
+            if settings['blockcall']:
+                fun = globals()[settings['functionname']]
             else:
-                fun = globals()[fun_name+"T"]
-            SetupShortcutAndHandle(fun, scriptname, key, shiftkey, ctrlkey, altkey)
+                fun = globals()[settings['functionname']+"T"]
+            SetupShortcutAndHandle(fun, settings)
     loaded = True
 
