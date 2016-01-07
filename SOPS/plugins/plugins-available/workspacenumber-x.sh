b7f7c38cbabcb38845f9b7dfc1223e93330df306
Storm dragon <stormdragon2976@gmail.com>
2016-01-07 13:45:38 -0500

    Added check for xprop, changed output and number to match what orca says when switching desktops.

--- SOPS/plugins/plugins-available/workspacenumber-x.sh
+++ SOPS/plugins/plugins-available/workspacenumber-x.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #for this you need xprop
 #it outputs the current workspace no
-echo "Destkop "
-xprop -root _NET_CURRENT_DESKTOP | tail -c 3
+if ! hash xprop &> /dev/null ; then
+echo "This plugin requires the package xprop to function."
+exit 1
+fi
+echo "Workspace $(($(xprop -root _NET_CURRENT_DESKTOP | tail -c 3) + 1))"
+exit 0
