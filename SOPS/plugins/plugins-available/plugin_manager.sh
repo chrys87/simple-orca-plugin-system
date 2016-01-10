1dfdc5314b66fe8a0dacfa16739377cd54559862
chrys <chrys87@web.de>
2016-01-10 17:20:25 +0100

    add commands to plugin_manager

--- SOPS/plugins/plugins-available/plugin_manager.sh
+++ SOPS/plugins/plugins-available/plugin_manager.sh
@@ -95,8 +95,10 @@ if [ -z "$items" ]; then
 exit 0
 fi
 local keyList=""
-checkList="$(echo {a..z} {0..9} | sed 's/\([a-z]\)/FALSE \1/g')"
-checkList="FALSE alt FALSE control FALSE shift $checkList"
+checkList="$(echo {a..z} {0..9} | sed 's/\([a-z]\)/FALSE \1/g')" #Keys
+checkList="FALSE alt FALSE control FALSE shift $checkList" #Modifier
+checkList="FALSE startnotify FALSE stopnotify FALSE showstderr FALSE parameters FALSE blockcall $checkList" #commands
+
 for i in $items ; do
 fileName="$(zenity --list --title "Simple Orca Plugin Manager" --text "Select keyboard shortcut for $i:" --checklist --separator __+__ --column "" --column "" $checkList)"
 if [ -z "$fileName" ]; then
