9f52121522ba6994e82365ecb43f3b349b85f4a9
chrys <chrys87@web.de>
2016-07-01 16:49:11 +0200

    add posibility to define propertys in the file

--- SOPS/examples/loadmodule_test__-__exec__+__loadmodule.py
+++ SOPS/examples/loadmodule_test__-__exec__+__loadmodule.py
@@ -3,6 +3,9 @@
 # an example for import an advanced plugin
 # loaded just once on starting orca (exec)
 # change setting chname for !, let orca announce "bang" instead of !
+
+#the following could be done to specify SOPS propertys in the file
+#sopsproperty:loadmodule
 import orca.orca # Imports the main screen reader
 orca.chnames.chnames["!"] = "bang"
 
