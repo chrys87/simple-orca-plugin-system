dec5b4b1f9af032e77659c72efc67521f1199b78
Sebastian Humenda <shumenda@gmx.de>
2018-05-01 13:31:30 +0200

    maintainance changes
    -   remove executable bit from library file
    -   fix syntax error in examples

--- examples/silence__-__supressoutput__+__key_y.sh
+++ examples/silence__-__supressoutput__+__key_y.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # supressoutput test, you would just hear nothing :).
-echo "This is a test that you would not hear, because the plugin is silenced.
+echo "This is a test that you would not hear, because the plugin is silenced."
